b4a93bc3b7cd2313cf4f4f5bf38e15378a6a56ae
Eli Holzman <eli770440@gmail.com>
2025-05-28 18:33:59 +0300

    for gh-p

--- src/RoutesC.js
+++ src/RoutesC.js
@@ -1,13 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage.js';
-// import JSProjects from './pages/JSProjects';
 
 const RoutesC = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                {/* <Route path="/js_projects" element={<JSProjects />} /> */}
             </Routes >
         </div>
     )
